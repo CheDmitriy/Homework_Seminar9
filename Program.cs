c1e5c6d20356f4cb7ea3d2f75080ee8d4ccf7fd4
DC <dimitriitdi@gmail.com>
2022-07-26 17:26:05 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿/*int Count (int n)  // Задача 64
+{
+    if (n/10!=0)
+
+        return 1 + Count(n/10);
+    else
+
+        return 1;
+}
+
+Console.WriteLine(Count(0));*/
+
+
+Console.WriteLine("Enter the initial number m:");  // Задача 66
+int m = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Enter the initial number n:");
+int n = Convert.ToInt32(Console.ReadLine());
+
+void SumofNumber (int m, int n, int sum)
+{
+    if (m > n) 
+    {
+        Console.WriteLine($"The sum of natural elements in the range from m to n: {sum}"); 
+        return;
+    }
+    sum = sum + (m++);
+    SumofNumber(m, n, sum);
+}
+
+SumofNumber(m, n, 0);
